bd559fed0f51e24faa7efe7c606b3fe3998c3f45
Marek Pisarek <marekpisarek94@gmail.com>
2019-11-19 19:15:14 +0100

    Added parts of lab

---
+++ 1/Maven/spring/src/main/java/com/app/spring/App.java
@@ -0,0 +1,12 @@
+package com.app.spring;
+
+import org.springframework.stereotype.Controller;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
